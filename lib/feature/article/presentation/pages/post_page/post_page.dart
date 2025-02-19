2f10c6690fcac0adbb6d5d9518d20ca1eba64ec1
Gleb <mikhalkou_ha@st.by>
2025-02-19 18:54:05 +0300

    Add base post page, refactor rest client options

--- lib/feature/article/presentation/pages/post_page/post_page.dart
+++ lib/feature/article/presentation/pages/post_page/post_page.dart
@@ -1,17 +1,71 @@
+import 'package:art_list/core/presentation/provider/theme_provider.dart';
+import 'package:art_list/di/modules/configure_dependencies.dart';
+import 'package:art_list/feature/article/presentation/bloc/post_bloc/post_bloc.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
-class PostPage extends StatelessWidget {
+class PostPage extends StatefulWidget {
   const PostPage({super.key});
 
   @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  @override
+  void initState() {
+    super.initState();
+    Injector.locator.resolve<PostBloc>().add(GetListPost());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Text(
-          'Post page',
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List posts'),
+        actions: [
+          IconButton(
+            onPressed: () => context.read<ThemeProvider>().switchingTheme(),
+            icon: Icon(
+              Icons.dark_mode_outlined,
+            ),
+          )
+        ],
+      ),
+      body: BlocBuilder<PostBloc, PostState>(
+        bloc: Injector.locator.resolve<PostBloc>(),
+        builder: (context, state) {
+          if (state is Error) {
+            return Center(
+              child: Text(state.message),
+            );
+          } else if (state is Loaded) {
+            return Center(
+              child: Expanded(
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: state.posts.length,
+                  itemBuilder: (context, index) {
+                    final post = state.posts[index];
+                    return ListTile(
+                      title: Text(post.title),
+                      subtitle: Text(
+                        post.body,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    );
+                  },
+                  separatorBuilder: (_, __) => const SizedBox(height: 12),
+                ),
+              ),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
